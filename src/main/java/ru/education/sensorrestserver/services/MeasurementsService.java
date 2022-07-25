1f803acf1e7830b6e4cbf714d234db38a50700fa
MrOneThree <k.popov.inbox@yandex.ru>
2022-07-25 20:06:25 +0300

    Implemented rainyDaysCount

--- src/main/java/ru/education/sensorrestserver/services/MeasurementsService.java
+++ src/main/java/ru/education/sensorrestserver/services/MeasurementsService.java
@@ -6,6 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.education.sensorrestserver.models.Measurement;
 import ru.education.sensorrestserver.repo.MeasurementRepo;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -23,8 +26,18 @@ public class MeasurementsService {
     }
 
     public long rainyDaysCount(){
+        Calendar cal = Calendar.getInstance();
         return  repo.findAll().stream()
                 .filter(Measurement::getRaining)
+                .map(measurement -> {
+                    cal.setTimeInMillis(measurement.getTimestamp());
+                    cal.set(Calendar.HOUR_OF_DAY, 0);
+                    cal.set(Calendar.MINUTE, 0);
+                    cal.set(Calendar.SECOND, 0);
+                    cal.set(Calendar.MILLISECOND, 0);
+                    return cal.getTime();
+                })
+                .distinct()
                 .count();
     }
 
